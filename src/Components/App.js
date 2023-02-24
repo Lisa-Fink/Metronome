e2cc59504e57d25ac90e55f0a198a80c1aa4c9af
lisa fink <lisadfink@gmail.com>
2023-02-24 17:40:16 -0500

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import Metronome from "./Metronome";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Metronome />
+      </main>
+    </div>
+  );
+}
+
+export default App;
