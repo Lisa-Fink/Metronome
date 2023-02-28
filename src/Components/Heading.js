2bc8706793978f5d7ec3d052762eab1db2b326d7
lisa fink <lisadfink@gmail.com>
2023-02-27 21:56:48 -0500

    Added light/dark icon and h1

---
+++ src/Components/Heading.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+import { IoMusicalNotesOutline } from "react-icons/io5";
+import "../styles/Header.css";
+
+function Heading() {
+  const [lightMode, setLightMode] = useState(false);
+
+  useEffect(() => {
+    if (lightMode === true) {
+      toggleColorScheme("light-mode");
+    } else {
+      toggleColorScheme("dark-mode");
+    }
+  }, [lightMode]);
+
+  const toggleLightMode = () => {
+    setLightMode(!lightMode);
+  };
+
+  const toggleColorScheme = (scheme) => {
+    document.documentElement.classList.remove("dark-mode", "light-mode");
+    document.documentElement.classList.add(scheme);
+  };
+  return (
+    <header>
+      <h1>
+        Super Metronome <IoMusicalNotesOutline />
+      </h1>
+      <div id="light-mode">
+        {lightMode ? (
+          <MdOutlineDarkMode onClick={toggleLightMode} />
+        ) : (
+          <MdOutlineLightMode onClick={toggleLightMode} />
+        )}
+      </div>
+    </header>
+  );
+}
+
+export default Heading;
