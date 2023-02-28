330fd32a04687ad242a83ac32eefa44ddff80178
lisa fink <lisadfink@gmail.com>
2023-02-27 21:56:35 -0500

    Moved header to heading component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,32 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Metronome from "./Metronome";
+import Heading from "./Heading";
 import "../styles/App.css";
-import { MdOutlineLightMode } from "react-icons/md";
 
 function App() {
-  const [lightMode, setLightMode] = useState(false);
-
-  useEffect(() => {
-    if (lightMode === true) {
-      toggleColorScheme("light-mode");
-    } else {
-      toggleColorScheme("dark-mode");
-    }
-  }, [lightMode]);
-
-  const toggleLightMode = () => {
-    setLightMode(!lightMode);
-  };
-
-  const toggleColorScheme = (scheme) => {
-    document.documentElement.classList.remove("dark-mode", "light-mode");
-    document.documentElement.classList.add(scheme);
-  };
   return (
     <div className="App">
-      <header>
-        <MdOutlineLightMode onClick={toggleLightMode} />
-      </header>
+      <Heading />
       <main>
         <Metronome />
       </main>
