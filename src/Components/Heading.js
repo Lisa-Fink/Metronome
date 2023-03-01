9d833c243206415e844923b8c28d8ef2d98c7419
lisa fink <lisadfink@gmail.com>
2023-03-01 14:00:50 -0500

    Updated heading name and spacing

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -26,7 +26,7 @@ function Heading() {
     <header>
       <div className="heading-div">
         <h1>
-          Super Metronome <IoMusicalNotesOutline />
+          My Custom Metronome <IoMusicalNotesOutline />
         </h1>
         <div id="light-mode">
           {lightMode ? (
