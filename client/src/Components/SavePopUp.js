10f2517f57a375c0ebda4271145cf6e4fc06bf92
lisa fink <lisadfink@gmail.com>
2023-03-18 22:10:39 -0400

    Created pop up for saving new metronomes

---
+++ client/src/Components/SavePopUp.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+function SavePopUp({ title, setTitle, setUserPopUp, saveFunc, setError }) {
+  const [newTitle, setNewTitle] = useState("");
+
+  const handleClose = (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+
+    setUserPopUp(false);
+  };
+
+  const handleTitleChange = (e) => {
+    setNewTitle(e.target.value);
+  };
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const old = title;
+    const savedTitle =
+      newTitle == "" ? `Untitled ${Date.now().slice(0, 10)}` : newTitle;
+    setTitle(savedTitle);
+    try {
+      await saveFunc(savedTitle);
+    } catch (error) {
+      console.log(error);
+
+      setError("Error Saving Metronome");
+      setTitle(old);
+    }
+    setUserPopUp(false);
+  };
+
+  return (
+    <div className="pop-up-container">
+      <div className="pop-up">
+        <div id="x-container">
+          <AiOutlineClose id="x-icon" onClick={handleClose} />
+        </div>
+        <h2>Save</h2>
+        <form onSubmit={handleSave}>
+          <label>
+            Title
+            <div className="input-div">
+              <input
+                value={newTitle}
+                onChange={handleTitleChange}
+                placeholder="Title"
+              />
+            </div>
+          </label>
+          <div className="button-div">
+            <button type="submit">Save</button>
+            <button onClick={handleClose}>Cancel</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SavePopUp;
