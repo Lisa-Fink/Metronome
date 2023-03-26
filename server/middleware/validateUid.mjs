6f2b0fd59202c63f6682f10c59e6c2e5993db3cf
lisa fink <lisadfink@gmail.com>
2023-03-26 12:44:34 -0400

    Moved acct info to .env

--- server/middleware/validateUid.mjs
+++ server/middleware/validateUid.mjs
@@ -1,10 +1,15 @@
 import "dotenv/config";
 import admin from "firebase-admin";
 
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+  }),
 });
 
 const validateUid = (req, res, next) => {
