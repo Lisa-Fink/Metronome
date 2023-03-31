6e30ba6a42b216ae3235f0df74a262de465cc91a
lisa fink <lisadfink@gmail.com>
2023-03-31 00:07:06 -0400

    Converted to use scheduler function

--- client/src/utils/metronome/samples/countInPlayer.js
+++ client/src/utils/metronome/samples/countInPlayer.js
@@ -8,46 +8,69 @@ const countInPlayer = ({
   subdivide,
   volumeRef,
   countIn,
-  setTimerId,
   audioCtx,
   stopCheck,
 }) => {
+  let beat = 0;
+  let startTime = 0;
+  let isPlaying = false;
+  let end = false;
+  let schedulerId = null;
+  let addToStart;
+
   const loadCountIn = async () => {
     return await fetchAudio(audioSamples.Triangle.downBeats, audioCtx);
   };
 
-  const playCountIn = async (start) => {
-    setIsPlaying(true);
-    // use triangle down beat
-    const clickBuffer = await loadCountIn(audioCtx.current);
-
-    const addToStart = 60 / (bpm * subdivide);
-    const interval = ((60 / (bpm * subdivide)) * 1000) / 1.5;
-    let beat = 0;
-    let startTime = start;
+  const scheduleNextClick = (clickBuffer, gainNode) => {
+    const now = audioCtx.current.currentTime;
+    while (startTime < now + 0.5) {
+      const source = audioCtx.current.createBufferSource();
+      source.buffer = clickBuffer;
+      gainNode.gain.value = volumeRef.current;
+      source.connect(gainNode);
+      source.start(startTime);
+      startTime += addToStart;
+      beat++;
+      if (beat === countIn * timeSignature * subdivide) {
+        end = startTime;
+        break;
+      }
+      if (stopCheck()) {
+        end = -1;
+        break;
+      }
+    }
+    if (end !== false) {
+      clearTimeout(schedulerId);
+      return Promise.resolve(end);
+    }
     return new Promise((resolve) => {
-      const id = setInterval(() => {
-        const source = audioCtx.current.createBufferSource();
-        source.buffer = clickBuffer;
-        const gainNode = audioCtx.current.createGain();
-        gainNode.gain.value = volumeRef.current;
-        gainNode.connect(audioCtx.current.destination);
-        source.connect(gainNode);
-        source.start(startTime);
-        startTime += addToStart;
-        beat++;
-        if (beat == countIn * timeSignature * subdivide) {
-          clearInterval(id);
-          resolve(startTime);
-        }
-        if (stopCheck()) {
-          clearInterval(id);
-          resolve(-1);
-        }
-      }, interval);
-      setTimerId(id);
+      schedulerId = setTimeout(
+        () => resolve(scheduleNextClick(clickBuffer, gainNode)),
+        25
+      );
     });
   };
+
+  const playCountIn = async (start) => {
+    if (isPlaying) {
+      return;
+    }
+
+    setIsPlaying(true);
+    isPlaying = true;
+
+    const clickBuffer = await loadCountIn(audioCtx.current);
+    const gainNode = audioCtx.current.createGain();
+    gainNode.connect(audioCtx.current.destination);
+
+    addToStart = 60 / (bpm * subdivide);
+    startTime = startTime = start;
+
+    return await scheduleNextClick(clickBuffer, gainNode);
+  };
+
   return { playCountIn };
 };
 
